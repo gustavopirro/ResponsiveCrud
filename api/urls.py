1f0af56d3992591420190fcf4d919fe4b8e1530b
gustavopirro <gustavo.pirro@gmail.com>
2022-03-23 00:20:21 -0300

    feat: changed homepage template and create post page

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from api.views import CreatePostView
+from api.views import DetailPostView
 from api.views import ListPostsView
 
 app_name = 'api'
 
 urlpatterns = [
-    path('', CreatePostView.as_view(), name='post_create'),
-    path('posts/', ListPostsView.as_view(), name='post_list'),
+    path('', ListPostsView.as_view(), name='post_list'),
+    path('posts/create', CreatePostView.as_view(), name='post_create'),
+    path('posts/<int:pk>/', DetailPostView.as_view(), name='post_detail'),
 ]
