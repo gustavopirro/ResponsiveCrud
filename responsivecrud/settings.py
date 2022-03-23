db07fbc12bfdf99074d453cc9110b541c1dabc27
gustavopirro <gustavo.pirro@gmail.com>
2022-03-23 05:18:42 -0300

    change: changed redirect logout url

--- responsivecrud/settings.py
+++ responsivecrud/settings.py
@@ -111,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Login Settings
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
